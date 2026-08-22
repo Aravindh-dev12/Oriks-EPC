9d4e2dc2b90c937d40ab676551c56e042166cd07
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2026-08-23 00:45:03 +0530

    Load refined homepage styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import ModernRouter from './ModernRouter';
 import './styles.css';
-import './oriks-home.css';
-import './home-modern.css';
-import './home-realistic.css';
+import './home-final.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
