85d9974468fc3ba03b9a2a3f55918db9d271aed6
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2026-09-04 18:05:22 +0530

    Load homepage navigation polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ModernRouter from './ModernRouter';
 import './styles.css';
 import './home-final.css';
+import './home-link-fixes.css';
 import './corporate-pages.css';
 import './energy-footer-final.css';
 
