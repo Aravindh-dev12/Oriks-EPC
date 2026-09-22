25ccfac84f48b889eb5934fb3908a89ba65afd9c
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2026-09-22 09:06:32 +0530

    Fix root router to render the updated Propcare homepage

--- src/ModernRouter.tsx
+++ src/ModernRouter.tsx
@@ -1,8 +1,5 @@
-import { useLocation } from 'react-router-dom';
 import App from './App';
-import HomePurple from './HomePurple';
 
 export default function ModernRouter() {
-  const location = useLocation();
-  return location.pathname === '/' ? <HomePurple /> : <App />;
+  return <App />;
 }
